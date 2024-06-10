86210dd2a7995ba84c55b9ea283a30d0993280c2
Wojciech Balda <wojtek.balda@gmail.com>
2024-06-10 11:36:22 +0200

    Change middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,4 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware();
+export { auth as middleware } from "@/auth"
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
