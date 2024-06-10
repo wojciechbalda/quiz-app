1df97a7a789274c6e1dddd902887cf4029891ab8
Wojciech Balda <wojtek.balda@gmail.com>
2024-06-10 11:33:47 +0200

    Create generatePagination function

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,29 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const generatePagination = (currentPage: number, totalPages: number) => {
+  const placeholder = "..." as const
+  if (totalPages <= 7) {
+    return Array.from({ length: totalPages }, (_, i) => i + 1);
+  }
+
+  if (currentPage < 4) {
+    return [1, 2, 3, 4, placeholder, totalPages - 1, totalPages];
+  }
+
+  if (currentPage > totalPages - 3) {
+    return [1, 2, placeholder, totalPages-3, totalPages - 2, totalPages - 1, totalPages];
+  }
+
+  return [
+    1,
+    placeholder,
+    currentPage - 1,
+    currentPage,
+    currentPage + 1,
+    placeholder,
+    totalPages,
+  ];
+};
+
