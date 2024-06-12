2e4826cd7bf32c363c225c4e99bc39b772e1ae58
Wojciech Balda <wojtek.balda@gmail.com>
2024-06-12 13:21:50 +0200

    Add trust host option in auth file

--- auth.ts
+++ auth.ts
@@ -7,6 +7,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Github],
   adapter: PrismaAdapter(prisma),
   session: {strategy: 'jwt'},
+  trustHost: true,
   callbacks: {
     jwt({token, user})
     {
